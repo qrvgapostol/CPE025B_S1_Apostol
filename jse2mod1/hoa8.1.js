4cd98a1a5f4faae1c0c1b36619de6faf6fd83dd2
Ruud Van G. Apostol <qrvgapostol@tip.edu.ph>
2026-03-06 15:42:19 +0800

    Done

---
+++ jse2mod1/hoa8.1.js
@@ -0,0 +1,62 @@
+class Vehicle {
+    constructor(id, type) {
+      this.id = id;
+      this.type = type;
+      this.isDeployed = false;
+    }
+  }
+  
+  class FleetManager {
+    constructor() {
+      this.vehicles = [];
+    }
+  
+    addVehicle(vehicle) {
+      this.vehicles.push(vehicle);
+    }
+  
+    deployVehicle(id) {
+      const vehicle = this.vehicles.find(v => v.id === id);
+  
+      if (!vehicle) {
+        return undefined;
+      }
+  
+      vehicle.isDeployed = true;
+      return vehicle;
+    }
+  
+    getAvailableVehicles() {
+        return this.vehicles.filter(v => v.isDeployed === false);
+     }
+    }
+
+    // Test Entity Instantiation
+    console.log("*** Test Entity Instantiation ***");
+    const testVehicle = new Vehicle("V01", "Truck");
+    console.log(testVehicle);
+    //Expect Vehicle object with id: V01, type: Truck, isDeployed: false
+
+    // Test Composition and Insertion
+    console.log("\n*** Test Composition and Insertion ***");
+    const fleet = new FleetManager();
+    fleet.addVehicle(testVehicle);
+    fleet.addVehicle(new Vehicle("V02","Van"));
+    fleet.addVehicle(new Vehicle("V03","Drone"));
+    console.log(fleet.vehicles.length);
+    // Expext: 3
+
+    //Test State Mutation
+    console.log("\n*** Test State Mutation ***");
+    fleet.deployVehicle("V02");
+    const deployVehicle = fleet.vehicles.find(v => v.id === "V02");
+    console.log(deployVehicle.isDeployed);
+    // Expect: true
+
+    //Test Data Filtering
+    console.log("\n*** Test Data Filtering ***");
+    const available = fleet.getAvailableVehicles();
+    console.log(available.length);
+    // Expect: 2
+    console.log(available.map(v => v.id));
+    //Expect: [V01, V03]
